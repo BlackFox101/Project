e1507bef9f7643ebb7ba5acd3c0049766e983a32
BlackFox101 <sad_alexxxei@@mail.ru>
2020-04-28 04:24:05 +0300

    Update

--- Project/Project/Data/Controllers/PersonController.cs
+++ Project/Project/Data/Controllers/PersonController.cs
@@ -40,6 +40,24 @@ namespace Project.Data.Controllers
             }
             return NotFound();
         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Person user = await db.Persons.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Person user)
+        {
+            db.Persons.Update(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
         [HttpGet]
         [ActionName("Delete")]
         public async Task<IActionResult> ConfirmDelete(int? id)
@@ -58,29 +76,15 @@ namespace Project.Data.Controllers
         {
             if (id != null)
             {
-                Person user = new Person { Id = id.Value };
-                db.Entry(user).State = EntityState.Deleted;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return NotFound();
-        }
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id != null)
-            {
                 Person user = await db.Persons.FirstOrDefaultAsync(p => p.Id == id);
                 if (user != null)
-                    return View(user);
+                {
+                    db.Persons.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
             }
             return NotFound();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Edit(Person user)
-        {
-            db.Persons.Update(user);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
         }
     }
 }
